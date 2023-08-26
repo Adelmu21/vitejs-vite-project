e966635516c04c73a0f66a0652c00122ec64bd67
Adelmu21 <109333026+Adelmu21@users.noreply.github.com>
2023-08-25 19:46:11 -0600

    1 file modified, 1 added, 1 deleted

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import "./index.css";
-import Root from "./routes/root";
+import Root from "./routes/root.jsx";
 
 const router = createBrowserRouter([
   {
@@ -15,7 +15,7 @@ const router = createBrowserRouter([
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  
     <RouterProvider router={router} />
-  </React.StrictMode>
+  
 );
