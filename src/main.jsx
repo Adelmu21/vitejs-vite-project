ebc103dec5702af398a5d00fee67865953d33955
Adelmu21 <109333026+Adelmu21@users.noreply.github.com>
2023-08-25 19:17:15 -0600

    1 file modified, 16 added, 1 deleted

---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './index.css'
+import Root from "./routes/root";
+
+// import Root, { rootLoader } from "./routes/root";
+// import Team, { teamLoader } from "./routes/team";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
